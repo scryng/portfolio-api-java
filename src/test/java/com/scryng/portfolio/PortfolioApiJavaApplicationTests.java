e7744d8cbb99f55677d53cead33857d9eb6c895e
scryng <125830775+scryng@users.noreply.github.com>
2026-06-28 21:06:26 -0300

    :tada: init: project setup with basic structure and configuration files

---
+++ src/test/java/com/scryng/portfolio/PortfolioApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scryng.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
