e5da65dd4c57aee39898efccf3fa0480719ebf2c
scryng <125830775+scryng@users.noreply.github.com>
2026-06-30 14:43:17 -0300

    :books: docs: add comments to ProjectStatus enum for clarity

--- src/main/java/com/scryng/portfolio/domain/enums/ProjectStatus.java
+++ src/main/java/com/scryng/portfolio/domain/enums/ProjectStatus.java
@@ -1,14 +1,14 @@
 package com.scryng.portfolio.domain.enums;
 
 public enum ProjectStatus {
-	UNDER_ANALYSIS,
-	ANALYSIS_COMPLETED,
-	ANALYSIS_APPROVED,
-	STARTED,
-	PLANNED,
-	IN_PROGRESS,
-	CLOSED,
-	CANCELLED;
+	UNDER_ANALYSIS, // em análise
+	ANALYSIS_COMPLETED, // análise realizada
+	ANALYSIS_APPROVED, // análise aprovada
+	STARTED, // iniciado
+	PLANNED, // planejado
+	IN_PROGRESS, // em andamento
+	CLOSED, // encerrado
+	CANCELLED; // cancelado
 
 	public boolean isDeletable() {
 		return this != STARTED && this != IN_PROGRESS && this != CLOSED;
